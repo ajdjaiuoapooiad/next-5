301810a8934b4a2be8664c380d52751fb3321344
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-25 23:40:23 +0900

    ok

--- src/app/tours/[id]/page.tsx
+++ src/app/tours/[id]/page.tsx
@@ -1,10 +1,24 @@
-const page = ({ params }: { params: { id: string } }) => {
-    console.log(params);
-  
-    return (
-      <div>
-        <h1 className='text-4xl'>ID : {params.id}</h1>
-      </div>
-    );
-  };
-  export default page;
+import mapsImg from '@/app/images/maps.jpg';
+import Image from 'next/image';
+const url = 'https://www.course-api.com/images/tours/tour-1.jpeg';
+
+const page = async ({ params }: { params: { id: string } }) => {
+  return (
+    <div>
+      <h1 className='text-4xl'>ID : {params.id}</h1>
+      <section className='flex gap-x-4 mt-4'>
+        <div>
+          <Image
+            src={mapsImg}
+            alt='maps'
+            width={192}
+            height={192}
+            className='w-48 h-48 object-cover rounded'
+          />
+          <h2>local image</h2>
+        </div>
+      </section>
+    </div>
+  );
+};
+export default page;
